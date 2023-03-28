9ac17262518f85f42282a13f87dd7c7da2df7d55
pawarsuyog330 <pawarsuyog330@gmail.com>
2023-03-28 18:39:40 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WishComponent } from './wish/wish.component';
+import { GreetComponent } from './greet/greet.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WishComponent,
+    GreetComponent
   ],
   imports: [
     BrowserModule,
